8863a7ff9daaa8076e0ad0a98045bb5320b4b659
SobamboS <sobamboolusola1152@gmail.com>
2023-04-10 00:08:20 +0100

    hello

---
+++ src/Array/Insertion.java
@@ -0,0 +1,17 @@
+package Array;
+
+// Inserting values into indexes pf an array
+import java.util.Arrays;
+
+public class Insertion{
+    public static void main(String[] args){
+
+
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
