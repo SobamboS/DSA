5a5e63e6c53d3d2c2ab7ea0c7eec55a2bcb58598
SobamboS <sobamboolusola1152@gmail.com>
2023-04-10 01:53:21 +0100

    Array

--- src/Array/Accessing.java
+++ src/Array/Accessing.java
@@ -3,25 +3,16 @@ public class Accessing{
     public static void main(String[] args){
 
 // Accessing element of an array
-        int [] slay = {1,23,4,5,6,6};
-      //  System.out.printf("Accessing %d:",slay[4]);
-
+        int[] slay={1,23,4,5,6,6};
+        System.out.printf("Accessing %d:",slay[4]);
 
 
         //Searching in array
         for(int i : slay){
-           // System.out.println(i);
+            System.out.println(i);
         }
 
         // Looping through multidimensional array
-
-int [][]first = {{1,2,3,4,5,6}, {2,3,4,5,7,8}};
-        for(int i =0; i < first.length; i++){
-            for(int j =0; j < first.length; i++){
-
-            System.out.println( first[i][j]);
-            }
-        }
 
     }
 }
